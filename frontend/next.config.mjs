5f655db942bffafbd599aa8ed43ad8a4063e3189
Dzyfhuba <hafidz21ub@gmail.com>
2024-04-04 18:47:54 +0700

    update docker setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: true,
+  images: {
+    formats: ['image/webp'],
+  },
+  // webpack(config) {
+  //   config.externals = {
+  //     'sharp': 'commonjs sharp'
+  //   }
+
+  //   return config
+  // },
 }
 
 export default nextConfig
